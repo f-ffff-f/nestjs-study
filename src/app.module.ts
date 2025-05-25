31520a381fdfdb53f300423d5b51399fa346b278
f-ffff-f <ghr95223@gmail.com>
2025-05-25 21:44:59 +0900

    comment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+// import { CatsController } from './cats/cats.controller'
 import { AdminController } from './admin/admin.controller'
 import { AccountController } from './account/account.controller'
 import { CatsService } from './cats/cats.service'
@@ -9,10 +10,18 @@ import { CatsModule } from './cats/cats.module'
 // 전체 애플리케이션의 모듈, 컨트롤러, 프로바이더(서비스 등) 관계의 시작점입니다.
 // AppModule 클래스가 모듈 클래스임을 나타냄. NestJS 런타임한테 알려주는 메타 데이터
 @Module({
-    // 루트모듈에서 모듈 임포트
+    // feature module 임포트
     imports: [CatsModule],
     // 여기에 넣어줘야 Nest가 컨트롤러를 인지한다.
-    controllers: [AppController, AdminController, AccountController],
-    providers: [AppService, CatsService],
+    controllers: [
+        AppController,
+        // CatsController,
+        AdminController,
+        AccountController,
+    ],
+    providers: [
+        AppService,
+        // CatsService
+    ],
 })
 export class AppModule {}
--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common'
 import { CatsController } from './cats.controller'
 import { CatsService } from './cats.service'
 
+// 특정 도메인과 관련된 구성요소들을 응집력 있는 단위로 묶는다.
+// 명시적으로 providers의 scope를 제어한다.(전역 프로바이더 방지, 명확한 의존성 관리)
 @Module({
     controllers: [CatsController],
     providers: [CatsService],
