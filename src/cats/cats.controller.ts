1d71807a02442d14b3b197ccef2de7d905d1c4fc
f-ffff-f <ghr95223@gmail.com>
2025-05-25 19:49:26 +0900

    scaffolding fin

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -11,12 +11,18 @@ import {
     Req,
 } from '@nestjs/common'
 import { Observable, of } from 'rxjs'
-import { CreateCatDto } from 'src/cats/create-cat.dto'
+import { CatsService } from 'src/cats/cats.service'
+import { CreateCatDto } from 'src/cats/dto/create-cat.dto'
+import { Cat } from 'src/cats/interfaces/cat.interface'
 
 // nest g controller cats 명령어로 생성된 컨트롤러 클래스. 자동으로 보일러 플레이트를 생성해줌.
 // @Controller('공통적으로 적용될 path prefix')
 @Controller('cats')
 export class CatsController {
+    // The CatsService is injected through the class constructor.
+    // private catsService: CatsService <--- 같은 줄에서 멤버로 선언하고 초기화 하는 단축코드
+    // IoC가 주입한다.
+    constructor(private catsService: CatsService) {}
     // private userSpecificData: string
     // 이런 식으로 요청별 데이터를 인스턴스 변수에 저장하면 안 됩니다! api endpoint는 stateless 해야 함.
     // 따라서 요청별 데이터를 저장하려면 요청 객체를 인자로 받아서 사용해야 함.
@@ -24,8 +30,8 @@ export class CatsController {
     @Post()
     // @HttpCode(204)
     // @Header('Cache-Control', 'no-store')
-    create(@Body() createCatDto: CreateCatDto): string {
-        return 'This action adds a new cat'
+    create(@Body() createCatDto: CreateCatDto): void {
+        this.catsService.create(createCatDto)
     }
     // @Get() 데코레이터는 findAll 메서드가 경로에 대한 HTTP GET 요청을 처리하는 핸들러 메서드임을 나타냄.
     // @Get()
@@ -39,20 +45,20 @@ export class CatsController {
     //     await new Promise((resolve) => setTimeout(resolve, 1000))
     //     return []
     // }
-    // @Get()
-    // findAll(): Observable<any[]> {
-    //     return of([])
-    // }
-
     @Get()
-    findAll(
-        @Query('age') age: number,
-        @Query('breed') breed: string
-    ): Observable<string> {
-        return of(
-            `This action returns all cats filtered by age: ${age} and breed: ${breed}`
-        )
+    findAll(): Observable<Cat[]> {
+        return of(this.catsService.findAll())
     }
+
+    // @Get()
+    // findAll(
+    //     @Query('age') age: number,
+    //     @Query('breed') breed: string
+    // ): Observable<string> {
+    //     return of(
+    //         `This action returns all cats filtered by age: ${age} and breed: ${breed}`
+    //     )
+    // }
 
     @Get(':id')
     findOne(@Param('id') id: string): string {
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { Cat } from 'src/cats/interfaces/cat.interface'
 
+// Injectable 데코레이터는 NestJS의 런타임 시스템(IoC: Inversion of Control 컨테이너)이 이를 관리하고 주입할 수 있음을 나타냄.
 @Injectable()
-export class CatsService {}
+export class CatsService {
+    private readonly cats: Cat[] = []
+
+    create(cat: Cat) {
+        this.cats.push(cat)
+    }
+
+    findAll(): Cat[] {
+        return this.cats
+    }
+}
